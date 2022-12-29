7dfe61bd02fa01c39a130ad6c23981c3f118a728
Duran11 <antonio11duran@gmail.com>
2022-12-29 14:56:47 -0600

    fixed bug in database.js with put request

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // done
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   console.log('PUT to database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
